9bd86708f44da64a7deb26e8b9d732951527a92f
runarvestmann <runarvestmann@gmail.com>
2023-02-19 10:11:17 +0000

    Initial commit

---
+++ 1 - First Website/script.js
@@ -0,0 +1,8 @@
+const textElement = document.getElementById("text");
+console.log(textElement);
+
+function changeColor() {
+  console.log("Button was clicked");
+  const textElement = document.getElementById("text");
+  textElement.style.fontSize = "32px";
+}
---
+++ 4 - RÚV Dagskrá/script.js
@@ -0,0 +1,24 @@
+const API_URL = "https://apis.is/tv/ruv";
+
+async function fetchData() {
+  const response = await fetch(API_URL);
+  const json = await response.json();
+
+  const container = document.getElementById("container");
+
+  const elementStringList = json.results.map(function (element) {
+    const startTime = new Date(element.startTime).toLocaleTimeString();
+    return `
+        <div>
+            <h2>${element.title}</h2>
+            <p>Upphafstími: ${startTime}</p>
+            <p>Lengd: ${element.duration}</p>
+            <p>Lýsing: ${element.description}</p>
+        </div>
+    `;
+  });
+
+  container.innerHTML = elementStringList.join("");
+}
+
+fetchData();
